a125ca566656dc604fd465a767cccf3efe43b963
DELL <sayedmhabibullah51@gmail.com>
2025-10-10 00:42:21 +0600

    I learn how to Loop in dictionary

---
+++ Loop in Dictionary.py
@@ -0,0 +1,28 @@
+Fruits={
+    'apple':{
+        'Maria':1,
+        'Shila':2,
+        'Depika':3,
+        'Halim':1
+    },
+ 'sells':{
+     'Banglabazar':'Dipok',
+     'Notunbazar':'Kalua',
+     'Borobazar':'Shabab',
+     'Massbazar':'Tanki',
+     'Cotobazar':'Faranki'
+ },
+    'Year':1987
+}
+
+for c in Fruits:
+    print(c)
+# Value dakhar Jonno
+for x in Fruits.values():
+    print(x)
+# Keys dakhar jonno
+for v in Fruits.keys():
+    print(v)
+# Items dakhar jonno
+for y in Fruits.items():
+    print(y)
