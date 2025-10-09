83da904e528282d3d0079f7e0210dc1322b09346
DELL <sayedmhabibullah51@gmail.com>
2025-10-10 00:58:49 +0600

    Learn how to copy items in dictionary

---
+++ Copy Dictionary.py
@@ -0,0 +1,14 @@
+Fruits={
+    'apple':{
+        'Maria':1,
+        'Shila':2,
+        'Depika':3,
+        'Halim':1
+    }
+}
+c=Fruits.copy()
+print(c)
+
+# dict diyao copy kora jay
+d=dict(Fruits)
+print(d)
