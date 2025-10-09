fa0731429be3428b5b2c28af08498bdf718957ba
DELL <sayedmhabibullah51@gmail.com>
2025-10-10 00:12:44 +0600

    Recently i learn how to remove items from dictionary

---
+++ Dictionary Remove Item.py
@@ -0,0 +1,19 @@
+Studentinfo={
+    'Shanto':{
+        'Address':'Oxford Mission Road',
+        'Home Name':'Mira bari',
+        'Number':123485994,
+        'House Number':4948,
+    },
+    'Rayhan':{
+        'Address': 'Oxford Mission Road, Diya bari',
+        'Home Name': 'Mira bari,9988',
+        'Number': 12348599959684,
+        'House Number': 49585848,
+    },
+    'Year':2018
+}
+Studentinfo.pop('Shanto')
+print(Studentinfo)
+Studentinfo.popitem()  # Remove the last item from the dictionary
+print(Studentinfo)
