58f79f2333547afc0af105fb710d4accc5a6ae2f
DELL <sayedmhabibullah51@gmail.com>
2025-10-11 00:08:10 +0600

    I learnt the basic concept of Function

---
+++ Function.py
@@ -0,0 +1,36 @@
+def addition(a,b,l):
+    sum= a+b,l
+addition(6,6,8787)
+addition(66,7,8776)
+addition(597,6778,776)
+addition(5555,4545,876)
+addition(8888888,8987877,765)
+
+def subtraction(c,d):
+    minus= c-d
+    print(minus)
+subtraction(76,98)
+subtraction(9,98)
+subtraction(99,876)
+subtraction(99,5689)
+subtraction(7688,677877)
+
+def multiplication(n,m):
+    muliple=n*m
+    print(muliple)
+multiplication(8688688788,697979797)
+multiplication(9697997979,606)
+multiplication(96960786868,46969)
+multiplication(6868698696997,6969696)
+multiplication(78675645432354657,76756544533)
+
+def division(g,h):
+    divi=g/h
+    print(divi)
+division(7,8)
+division(77,7777)
+division(65,666556)
+division(4545,565666)
+division(454545,5454545)
+
+
