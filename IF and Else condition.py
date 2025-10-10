a971ff5b8bba69c08beec6b914c6b558cbc909fd
DELL <sayedmhabibullah51@gmail.com>
2025-10-10 14:47:05 +0600

    Today i learn the basic concept of If and else

---
+++ IF and Else condition.py
@@ -0,0 +1,12 @@
+a= 94
+b= 90
+if a>b:
+    print('a boro b er theke')
+elif a<b:
+    print('a coto b er theke')
+elif a != b:
+    print('a ar b shoman na')
+elif a==b:
+    print('a ar b shoman')
+else:
+    print('a boro na b er theke')
