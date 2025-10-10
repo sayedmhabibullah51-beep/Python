33d2458d789e15ceb783a40a54fbd560ec4c4343
DELL <sayedmhabibullah51@gmail.com>
2025-10-10 18:37:33 +0600

    Recently i learn the While Loop

---
+++ While Loop.py
@@ -0,0 +1,11 @@
+# WHILE LOOP
+shanto=[3,45,5,66,7,7,8,99,0,8666,66]
+i=0
+while i<len(shanto):
+    print(shanto[i],shanto)
+    i+=1
+Shanto=0
+while Shanto<20:
+    print("Shanto is greater than that",Shanto)
+    Shanto+=1
+
