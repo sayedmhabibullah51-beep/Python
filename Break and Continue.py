87cebf5aee4535e3202ef4202bbd84b255e5db5e
DELL <sayedmhabibullah51@gmail.com>
2025-10-12 18:37:35 +0600

    I learnt the Break and Continue of loop

---
+++ Break and Continue.py
@@ -0,0 +1,17 @@
+Fruits=[1,2,3,4,5,6,7,8,9,10]
+for x in range(len(Fruits)): # range(len()) Ayta use korsi index dakhar jonno
+    if x==5:
+        break
+    print(x)
+
+Name = ['Shanto', 'Prodip', 'Pranto', 'Apple', 'Avocado', 'Strawbery']
+for i in Name:
+    if i=='Apple':
+        break
+    print(i)
+
+Name=['Shanto', 'Prodip', 'Pranto', 'Apple', 'Avocado', 'Strawbery']
+for d in Name:
+    if d=='Apple':
+        continue
+    print(d)
