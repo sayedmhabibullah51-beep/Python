8353396cda374c67c01d832c850c0d4c5c044943
DELL <sayedmhabibullah51@gmail.com>
2025-10-13 13:12:55 +0600

    I learnt the zip function and the basic concept of Recurtion

---
+++ RECURTION.py
@@ -0,0 +1,12 @@
+# RECURTION LIMILT 1000 PROJONTO
+
+def Refun():
+    print('Shanto')
+    Refun()
+Refun()
+
+#  ZIP FUNCTION
+list1=['Shakil','Shanto','Prodip']
+list2=['programming club','google','ms']
+print(list(zip(list1,list2)))
+print(tuple(zip(list1,list2)))
