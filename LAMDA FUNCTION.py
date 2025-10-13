908bb36795a29b99055ff9f37ce8d985151db6ee
DELL <sayedmhabibullah51@gmail.com>
2025-10-14 00:28:22 +0600

    Recently I learn the Lamda Function

--- LAMDA FUNCTION.py
+++ LAMDA FUNCTION.py
@@ -10,5 +10,7 @@ x= lambda a,b,c:a+b+c  # It uses the syntax: lambda arguments: expression.
 print(x(10,20,4))
 x= lambda x,y,z:x*y*z
 print(x(4,5,6))
-v= lambda m,n,b:m-n-b
-print(v(66,77,88))
+v= lambda m,n,b,g,j:(m-n-b)*(g/j)
+print(v(66,77,88,99,988))
+m=lambda q,w,e,r,t,y:(q+w+e)*(r*t/y)
+print(m(22,33,44,55,66,67))
