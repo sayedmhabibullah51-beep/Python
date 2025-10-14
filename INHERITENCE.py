5165d0df42e340675c8e1b548ccc4acbda8e1ee1
DELL <sayedmhabibullah51@gmail.com>
2025-10-14 21:38:44 +0600

    I learnt the Inheritence

---
+++ INHERITENCE.py
@@ -0,0 +1,33 @@
+class Baba:
+    Car='BMW'
+    Money='1000 Crore'
+    Bari='Tajmahal'
+
+class kaka(Baba):
+    Bhangabari='KDC'
+    Bhangagari='Rickshaw'
+
+k=kaka()
+print(k.Car)
+print(k.Bari)
+
+# Example
+
+class Shanto:
+    Bari='Black Pearl'
+    Car='BMW'
+    Tk='1000000 Billion'
+    GF='Aktao Nai'
+
+class Toky(Shanto):
+    Vadaima=''
+    BC=''
+
+t=Toky()
+print(t.Bari)
+print(t.Car)
+print(t.Tk)
+print(t.GF)
+
+
+
