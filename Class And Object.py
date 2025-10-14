3eadc9ea0dbfec4b150cf05d054c36a4669e0c6f
DELL <sayedmhabibullah51@gmail.com>
2025-10-14 13:20:28 +0600

    I learnt the Class And Object

---
+++ Class And Object.py
@@ -0,0 +1,20 @@
+class Shanto():
+    Name=""               #Shanto hoise Class er nam ar Name hoitey Mobile projonto agula hoisey class er property
+    Number=""
+    Gender=""
+    Food=""
+    Mobile=""
+
+a= Shanto()
+b= Shanto()
+
+a.Name='Shakil'
+a.Number=1234325965
+a.Gender='Male'
+a.Food='Burger'
+b.Mobile='Ifinix'
+print(a.Name)
+print(a.Number)
+print(a.Gender)
+print(a.Food)
+print(b.Mobile)
