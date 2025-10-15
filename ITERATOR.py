674421cae5710111c3ec699cd109130acbf2d7eb
DELL <sayedmhabibullah51@gmail.com>
2025-10-15 19:32:07 +0600

    Recently I learn Iterator

---
+++ ITERATOR.py
@@ -0,0 +1,14 @@
+# ITERATOR er kaj holo part part korey output dakhabey
+
+list=[1,2,3,4,5,6,7,'Shanto','Kamal','Jorina','Lotifa']
+
+for i in list:         #LOOP chalailey amara akshthe output dakhtey pari
+     print(i)
+
+x=iter(list)           #But ITERATOR vag vag koirra dakhabey
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
