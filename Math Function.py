5d3981783e236a97bfa3df6fbfb943ceb09db1b7
DELL <sayedmhabibullah51@gmail.com>
2025-10-18 00:42:36 +0600

    Recently I learn Math  Function in python

---
+++ Math Function.py
@@ -0,0 +1,11 @@
+a=min(10,22,13,1,33,44,47)
+print(a)
+
+b=max(100,22,3333,4444,5554,44443,33334,55,566,666,77)
+print(b)
+
+c=abs(-2.09)  # abs() function Minus (-) sign key Plus korbe
+print(c)
+
+d=pow(2,3) # pow() function Power korbe 2*2*2
+print(d)
