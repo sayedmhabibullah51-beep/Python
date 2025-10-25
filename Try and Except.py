df7b81e59101276ac4a24522d7f8f11e6243a9f7
DELL <sayedmhabibullah51@gmail.com>
2025-10-26 00:24:54 +0600

    Today I learn Try and Except method

---
+++ Try and Except.py
@@ -0,0 +1,10 @@
+print('My name is Shanto')
+try:
+    print(v)
+except  NameError :               # Areekta error lakhar jonno except er shathey NameError lakha lagbey
+    print('There is a problem')
+except:
+    print('Its not a error')
+print('This is a cow')
+print(1,2,3,4,5,6,7,4)
+print('This is a complex number',1j)
