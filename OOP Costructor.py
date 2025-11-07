2493e19f60be0d218ec0bb8618d433793eba7470
DELL <sayedmhabibullah51@gmail.com>
2025-11-07 19:33:07 +0600

    Rececntly I learn the Constructor of Object Oriented Programming

---
+++ OOP Costructor.py
@@ -0,0 +1,34 @@
+# CONSTRUCTOR
+
+# Perametarized Constructor
+
+class Familyinfo():
+    def __init__(self,name,number):
+        print(f'My name is {name},My number is {number}')
+
+p1= Familyinfo('abcd',123345774)
+
+
+class Fruitshop():
+    def __init__(self,name,number):
+        print(f'The name of the fruit is {name},The number of the fruit is {number}')
+Fruitshop('apple',474)
+Fruitshop('Banana',543)
+Fruitshop('Cherrie',856)
+
+class Gym():
+    def __init__(self,name,number):
+        print(f'The name of the item is{name},The number of the item{number}')
+
+Gym('Burble',33)
+Gym('Belt',32)
+Gym('Machine',34)
+
+class Shop():
+    def __init__(self,name,number):
+        print(f'The name of the grosseries is {name},The number of the grossries is{number}')
+Shop('Maggie',3)
+Shop('Chocolate',20)
+Shop('7up',20)
+Shop('Bubble gum',56)
+Shop('Chips',15)
